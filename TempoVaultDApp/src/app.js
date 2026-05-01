98d3ef01e4fee3cdb930b689e65b0eaf0cd4393e
root <root@LAPTOP-M594FTM7.localdomain>
2026-05-01 04:36:55 -0400

    updated

--- TempoVaultDApp/src/app.js
+++ TempoVaultDApp/src/app.js
@@ -1,6 +1,6 @@
 // Replace these after running your deploy script.
-const TOKEN_ADDRESS = "PASTE_ERC20TOKEN_ADDRESS_HERE";
-const VAULT_ADDRESS = "PASTE_VAULT_ADDRESS_HERE";
+const TOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const VAULT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 let provider;
 let signer;
