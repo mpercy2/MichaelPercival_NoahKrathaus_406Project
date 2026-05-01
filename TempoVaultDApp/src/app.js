c87b15588a671d361a8104eaf92cc89627b378b4
root <root@LAPTOP-M594FTM7.localdomain>
2026-05-01 11:56:15 -0400

    updated

--- TempoVaultDApp/src/app.js
+++ TempoVaultDApp/src/app.js
@@ -1,4 +1,4 @@
-// Replace these after running your deploy script.
+// replace after running deploy script.
 const TOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const VAULT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
